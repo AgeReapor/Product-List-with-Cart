05751ef80b3d5311efd971a82b67166aa3e06e18
Adrian Reapor <diatum19@gmail.com>
2024-07-17 09:52:57 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      Hello World
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+      Coded by{" "}
+      <a href="https://github.com/AgeReapor" target="_blank">
+        Age Reapor
+      </a>
+      .
+    </div>
+  );
+}
